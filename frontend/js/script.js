e4c6206e72d0bb7692e6e4c6793cca6ec21647fd
Praveen <apnerve@gmail.com>
2012-05-01 20:45:15 +0530

    removed the conditional loaded for JS files and created the "create" square feature

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,5 @@
 Modernizr.load({
   load: [
-  'js/libs/bootstrap-modal.min.js',
   'js/libs/underscore-min.js',
   'js/libs/backbone-min.js',
   'js/libs/ICanHaz.min.js'
@@ -9,7 +8,7 @@ Modernizr.load({
     Modernizr.load({
       load: 'js/app.js',
       complete: function() {
-        $("#add-friend").click();
+        //$("#add-friend").click();        
       }
     });
   }
