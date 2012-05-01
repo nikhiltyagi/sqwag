fbe06110f3b1d510522e16673f0fc014414f89d7
Praveen <apnerve@gmail.com>
2012-05-01 11:01:05 +0530

    added lazy loader for loading JavaScript files

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,16 @@
-
-$(document).ready(function() {
-  $("#add-friend").click();
+Modernizr.load({
+  load: [
+  'js/libs/bootstrap-modal.min.js',
+  'js/libs/underscore-min.js',
+  'js/libs/backbone-min.js',
+  'js/libs/ICanHaz.min.js'
+  ],
+  complete: function() {
+    Modernizr.load({
+      load: 'js/app.js',
+      complete: function() {
+        $("#add-friend").click();
+      }
+    });
+  }
 });
-
-Modernizr.load('js/libs/bootstrap-modal.js');
