769debaf5f8daff12bc49dca120d72950d86caab
Vaibhav Saini <vaibps17@gmail.com>
2012-05-03 18:40:13 +0530

    removed info

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'vaibps17@gmail.com'
-EMAIL_HOST_PASSWORD = 'galaxys248'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 MANAGERS = ADMINS
@@ -20,7 +20,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'sqwag-dj',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'PASSWORD': 'root',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
