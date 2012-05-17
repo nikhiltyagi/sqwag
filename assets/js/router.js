937c2fbeca724079ed14b49cf2604a746d79d562
Vaibhav Saini <vaibps17@gmail.com>
2012-05-17 22:06:36 +0530

    adding router

---
+++ assets/js/router.js
@@ -0,0 +1,29 @@
+var router = {
+	init: function(context){
+		var self = this;
+
+		self.routes = {
+			"settings":{"template":{"url":"/assets/templates/settings.html","elementId":"#content"}},
+			"home" :  {"template":{"url":"/assets/templates/feed.html","elementId":"#content"}},
+			"logout" : {},
+			"xyz" : {}
+		};
+		// merging two objects.
+		if(null!=context && context.constructor==Object){
+			if(null !=context.routes && context.routes.constructor==Object){
+				$.extend(self.routes,context.routes);
+			}
+		}
+	},
+
+	routeTo : function(route){
+		var self = this;
+		var config = self.routes[route];
+		self.loadTemplate(config.template);
+		SQ.fetchFeed();
+	},
+	loadTemplate : function(template){
+		var self = this;
+		$(template.elementId).load(template.url);
+	}
+}
