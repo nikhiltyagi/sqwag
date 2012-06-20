c6aa418d045ef41c2c584f12c4fbb931c095869e
Vaibhav Saini <vaibps17@gmail.com>
2012-06-21 01:49:37 +0530

    PIL

---
+++ apache/sqwag.wsgi~
@@ -0,0 +1,6 @@
+import os, sys
+sys.path.append('/home/saini/workspace/sqwag/apache')
+sys.path.append('/home/saini/workspace/sqwag')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
---
+++ apache/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import patterns, include, url
+import settings
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^sqwag/', include('sqwag_api.urls')),
+    url(r'^api/', include('api.urls')),
+    url(r'^assets/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.CONTENT_BASE}),
+    url(r'^', include('sqwag_frontend.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
