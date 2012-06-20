1eb29298c250b4a9c94497b553b66fb5e4428e77
Vaibhav Saini <vaibps17@gmail.com>
2012-06-21 00:48:20 +0530

    apache setup

--- settings.py
+++ settings.py
@@ -122,7 +122,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'sqwag.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- urls.py
+++ urls.py
@@ -6,10 +6,10 @@ import settings
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^sqwag/', include('sqwag.sqwag_api.urls')),
-    url(r'^api/', include('sqwag.api.urls')),
+    url(r'^sqwag/', include('sqwag_api.urls')),
+    url(r'^api/', include('api.urls')),
     url(r'^assets/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.CONTENT_BASE}),
-    url(r'^', include('sqwag.sqwag_frontend.urls')),
+    url(r'^', include('sqwag_frontend.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
